c2623d15bc6497c44574771141837691cf35e082
jpablopachar <jpablopachar1993@gmail.com>
2023-11-30 18:08:09 -0500

    Creación del proyecto

---
+++ src/main/java/com/api/hateoas/appaccounts/AppAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.api.hateoas.appaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAccountsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/hateoas/appaccounts/AppAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.hateoas.appaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
